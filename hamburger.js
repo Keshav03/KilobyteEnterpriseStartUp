75130391d6185c2003ffca0dfe064ad2cb23b2c0
Keshav03 <60392137+Keshav03@users.noreply.github.com>
2020-10-07 07:29:38 -0700

    KB

---
+++ hamburger.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+const menuBtn = document.querySelector('menuBtn');
+let menuOpen = false;
+menuBtn.addEventListener('click',() => {
+	
+	if(!menuOpen){
+		menuBtn.classList.add('open');
+		menuOpen=true;
+	}else{
+		
+		menuBtn.classList.remove('open');
+		menuOpen=false;
+	}
+	
+});
